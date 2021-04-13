57b7c7ba86cfb952540bb93779fe1876c85a60fd
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2021-04-12 21:13:17 -0700

    copied .eslintr and .gitignore from class repo, made some changes to server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const cors = require('cors');
 
@@ -13,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 
 app.get('/', (request, response) => {
   response.send('hello!');
-})
+});
 
 // constructor function to create a Forecast object that takes in the parameter of "day"
 // defines the properties of the date and the weather description
@@ -23,10 +25,10 @@ function Forecast(day){
 }
 
 // creates an array of days by mapping over the weather data from weather.json and creating a new Forecast object for each item in the array
-// 
 app.get('/weather', (request, response) => {
   let weatherArray = weatherData.data.map(day => new Forecast(day));
-  response.json(weatherArray);
+  response.send(weatherArray);
+  // response.json(weatherArray);
 });
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
