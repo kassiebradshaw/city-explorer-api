4107e2c4d356affbb36ec2027a98d00cb4ac8473
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2021-04-14 20:22:01 -0700

    Lab 07 complete

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 
-require ('dotenv').config();
+require('dotenv').config();
 
 const weatherData = require('./data/weather.json');
 
@@ -19,16 +19,24 @@ app.get('/', (request, response) => {
 
 // constructor function to create a Forecast object that takes in the parameter of "day"
 // defines the properties of the date and the weather description
-function Forecast(day){
+function Forecast(day) {
   this.date = day.datetime;
   this.description = day.weather.description;
 }
 
 // creates an array of days by mapping over the weather data from weather.json and creating a new Forecast object for each item in the array
 app.get('/weather', (request, response) => {
-  let weatherArray = weatherData.data.map(day => new Forecast(day));
-  response.send(weatherArray);
-  // response.json(weatherArray);
+  try {
+    let weatherArray = weatherData.data.map(day => new Forecast(day));
+    response.send(weatherArray);
+  } catch (error) {
+    handleErrors(error, response);
+  }
 });
+
+
+function handleErrors(error, response) {
+  response.status(500).send('internal error');
+}
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
