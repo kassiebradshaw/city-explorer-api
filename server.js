1603f166a4ff6a709ea378684f07e712aeb517ec
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2021-04-14 23:17:48 -0700

    Trying to get request/response set up

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 require('dotenv').config();
 
-const weatherData = require('./data/weather.json');
+const weatherData = require('./data/weather.json'); // this is where the weather API call will be
 
 const app = express();
 
@@ -27,7 +27,7 @@ function Forecast(day) {
 // creates an array of days by mapping over the weather data from weather.json and creating a new Forecast object for each item in the array
 app.get('/weather', (request, response) => {
   try {
-    let weatherArray = weatherData.data.map(day => new Forecast(day));
+    let weatherArray = weatherData.data.map(day => new Forecast(day)); // we must use .body instead of .data when using superagent
     response.send(weatherArray);
   } catch (error) {
     handleErrors(error, response);
